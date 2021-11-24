99836b23fc2ca82aab16c5117094da257fe38d88
Gabriel Lazarov <gabriel.lazarov@gmail.com>
2021-11-24 22:36:09 +0200

    implemented data fetching

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 
 import FullModal from './components/Backrop/FullModal';
 import InfoPage from './components/InfoPage/InfoPage';
 
-const DUMMY_DATA = [
-  {
-    title: 'Cool',
-    description: '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    imgURL:
-      'https://images.unsplash.com/photo-1464817739973-0128fe77aaa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    title: 'Reason',
-    description: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    imgURL:
-      'https://images.unsplash.com/photo-1565700430899-1c56a5cf64e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80',
-  },
-  {
-    title: 'Safe',
-    description: '3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    imgURL:
-      'https://images.unsplash.com/photo-1580355275559-10c832e123f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-  },
-  {
-    title: 'Quiet',
-    description: '4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    imgURL:
-      'https://images.unsplash.com/photo-1634110555363-62469d636cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
-  },
-];
 
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [titleData, setTitleData] = useState([]);
+
+  useEffect(() => {
+    async function setdata() {
+      const response = await fetch(
+        'https://at-least-4-characters-long-default-rtdb.europe-west1.firebasedatabase.app/travelAgency/agencyName.json'
+      );
+      const data = await response.json();
+
+      const titles = [];
+      for (let key in data) {
+        titles.push({
+          description: data[key].description,
+          imgURL: data[key].imgURL,
+          title: data[key].title,
+        });
+      }
+
+      setTitleData(titles);
+    }
+    setdata();
+  }, []);
 
   const removeModal = () => {
     setModalIsVisible(false);
@@ -41,7 +37,7 @@ function App() {
     setModalIsVisible(true);
   };
 
-  const titles = DUMMY_DATA.map((el) => el.title);
+  const titles = titleData.map((el) => el.title);
 
   return (
     <Switch>
@@ -50,7 +46,11 @@ function App() {
           {modalIsVisible && (
             <FullModal titles={titles} clickHandler={removeModal} />
           )}
-          <InfoPage data={DUMMY_DATA.find((page) => page.title === title) } clickHandler={returnToModal} modalIsVisible={modalIsVisible}/>
+          <InfoPage
+            data={titleData.find((page) => page.title === title)}
+            clickHandler={returnToModal}
+            modalIsVisible={modalIsVisible}
+          />
         </Route>
       ))}
       <Route path="*">

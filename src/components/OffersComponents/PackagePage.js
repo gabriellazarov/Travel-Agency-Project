347a90ce47299c69a7efa134228f9d955a042d19
Gabriel Lazarov <gabriel.lazarov@gmail.com>
2022-02-03 00:28:55 +0200

    added date picker and logic

--- src/components/OffersComponents/PackagePage.js
+++ src/components/OffersComponents/PackagePage.js
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { useParams, Redirect } from 'react-router';
 import Navbar from '../SharedComponents/Navbar';
 
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
 import classes from './PackagePage.module.css';
 
 //this should be in a global store at some point
@@ -35,9 +39,28 @@ const DUMMY_VALID_OFFERS = [
   },
 ];
 
+const getUsefulDate = (date) => {
+  if (date === null) return '';
+  return date.toString().split(' ').slice(1, 4).join(' ');
+};
+
+function addDays(date, days) {
+  let result = date;
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
 //ADD LOGIC TO NAVIGATE AUTOMATICALLY TO LOGIN/REGISTER PAGE IF A REQUEST IS SENT AND WHEN ONE OF THE OPERATIONS IS DONE NAVIGATE AUTOMATICALLY BACK WITH SAVED INPUTS
 const PackagePage = () => {
   const [isLoading, setIsLoading] = useState(false);
+
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
 
   const chosenOffer = useParams().package;
 
@@ -53,8 +76,23 @@ const PackagePage = () => {
       <section>
         <form>
           <div>
-            <label htmlFor="startingDate">Choose a Starting Date</label>
-            <input type="date" id="startingDate" required />
+            <label htmlFor="startingDate">Choose a Date Range</label>
+            <ReactDatePicker
+              selected={startDate}
+              selectsRange
+              inline
+              startDate={startDate}
+              endDate={endDate}
+              onChange={onChange}
+              minDate={addDays(new Date(), 7)}
+              maxDate={addDays(new Date(), 365)}
+            />
+            <input
+              value={
+                startDate &&
+                `${getUsefulDate(startDate)} - ${getUsefulDate(endDate)}`
+              }
+            />
           </div>
           <div>
             <label htmlFor="location">Location</label>
